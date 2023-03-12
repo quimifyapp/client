1791cbcf4016824b8e34864bbe52346797b51053
Pablo Asenjo Navas-Parejo <pablo@quimify.com>
2023-03-12 17:41:15 +0100

    Switch from 'google.com' to 'example.com' as a connection testing address. It's owned by the Internet Corporation for Assigned Names and Numbers (ICANN) and can be used as a reliable internet connection checker.

--- lib/utils/internet.dart
+++ lib/utils/internet.dart
@@ -1,8 +1,13 @@
 import 'dart:io';
 
 Future<bool> checkInternetConnection() async {
+  // The domain 'example.com' is owned by the Internet Corporation for Assigned
+  // Names and Numbers (ICANN) and can be used as a reliable internet connection
+  // checker.
+  const String testingDomain = 'example.com';
+
   try {
-    final result = await InternetAddress.lookup('google.com');
+    final result = await InternetAddress.lookup(testingDomain);
     return result.isNotEmpty && result[0].rawAddress.isNotEmpty;
   } on SocketException catch (_) {
     return false;
