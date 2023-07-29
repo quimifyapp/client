81bdff0be4370fe3a9b4f3b1565853d8916b7ba2
Rubén Ortiz <rortmarb@g.educaand.es>
2023-07-29 21:48:02 +0200

    Primera versión hasInternetConnection

--- android/app/src/main/java/com/quimify/MainActivity.java
+++ android/app/src/main/java/com/quimify/MainActivity.java
@@ -1,6 +1,40 @@
 package com.quimify;
 
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.os.Build;
+import android.provider.Settings;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.RequiresApi;
+
 import io.flutter.embedding.android.FlutterActivity;
+import io.flutter.embedding.engine.FlutterEngine;
+import io.flutter.plugin.common.MethodChannel;
 
 public class MainActivity extends FlutterActivity {
+    private static final String CHANNEL = "airplane_mode_checker";
+
+    @RequiresApi(api = Build.VERSION_CODES.JELLY_BEAN_MR1)
+    @Override
+    public void configureFlutterEngine(@NonNull FlutterEngine flutterEngine) {
+        super.configureFlutterEngine(flutterEngine);
+
+        new MethodChannel(flutterEngine.getDartExecutor().getBinaryMessenger(), CHANNEL)
+                .setMethodCallHandler((call, result) -> {
+                    if (call.method.equals("isAirplaneModeOn")) {
+                        boolean isAirplaneModeOn = isAirplaneModeOn(this);
+                        result.success(isAirplaneModeOn);
+                    } else {
+                        result.notImplemented();
+                    }
+                });
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.JELLY_BEAN_MR1)
+    private boolean isAirplaneModeOn(Context context) {
+        return Settings.Global.getInt(context.getContentResolver(), Settings.Global.AIRPLANE_MODE_ON, 0) != 0;
+    }
 }
--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -2,12 +2,30 @@ import UIKit
 import Flutter
 
 @UIApplicationMain
-@objc class AppDelegate: FlutterAppDelegate {
-  override func application(
-    _ application: UIApplication,
-    didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
-  ) -> Bool {
-    GeneratedPluginRegistrant.register(with: self)
-    return super.application(application, didFinishLaunchingWithOptions: launchOptions)
-  }
+class AppDelegate: FlutterAppDelegate {
+    override func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        let controller = window.rootViewController as! FlutterViewController
+        let channel = FlutterMethodChannel(name: "airplane_mode_checker", binaryMessenger: controller.binaryMessenger)
+        channel.setMethodCallHandler { [weak self] (call, result) in
+            if call.method == "isAirplaneModeOn" {
+                let isAirplaneModeOn = self?.isAirplaneModeOn()
+                result(isAirplaneModeOn)
+            } else {
+                result(FlutterMethodNotImplemented)
+            }
+        }
+
+        GeneratedPluginRegistrant.register(with: self)
+        return super.application(application, didFinishLaunchingWithOptions: launchOptions)
+    }
+
+    private func isAirplaneModeOn() -> Bool {
+        return UIApplication.shared.isAirplaneModeEnabled
+    }
+}
+
+extension UIApplication {
+    var isAirplaneModeEnabled: Bool {
+        return !UIApplication.shared.isNetworkActivityIndicatorVisible
+    }
 }
