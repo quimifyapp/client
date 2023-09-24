69ad7a3c3b7088869b38c35a2935a63290a9fb2d
Filipe Barroso <filipe.barroso@gmail.com>
2023-09-24 22:09:17 +0100

    Add QuimifyScaffold Responsive feature

--- lib/pages/widgets/quimify_scaffold.dart
+++ lib/pages/widgets/quimify_scaffold.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:quimify_client/api/ads.dart';
 import 'package:quimify_client/pages/widgets/appearance/quimify_gradient.dart';
+import 'package:responsive_framework/responsive_breakpoints.dart';
+import 'package:responsive_framework/responsive_framework.dart';
 import 'package:signed_spacing_flex/signed_spacing_flex.dart';
 
 class QuimifyScaffold extends StatefulWidget {
@@ -35,47 +37,193 @@ class _QuimifyScaffoldState extends State<QuimifyScaffold> {
     if (widget.showBannerAd && !_loadedBannerAd) {
       _loadBannerAd(MediaQuery.of(context).size.width);
     }
+final small = MobileSmallView(
+      bodyRoundedCornersRadius: _bodyRoundedCornersRadius,
+      header: widget.header,
+      body: widget.body,
+      loadedBannerAd: _loadedBannerAd,
+      bannerAd: _bannerAd,
+    );
+    final mobile = MobileView(
+      bodyRoundedCornersRadius: _bodyRoundedCornersRadius,
+      header: widget.header,
+      body: widget.body,
+      loadedBannerAd: _loadedBannerAd,
+      bannerAd: _bannerAd,
+    );
 
     return Scaffold(
       resizeToAvoidBottomInset: false, // To avoid keyboard resizing
       backgroundColor: Theme.of(context).colorScheme.background,
-      body: SignedSpacingColumn(
-        spacing: -_bodyRoundedCornersRadius,
-        stackingOrder: StackingOrder.lastOnTop,
-        children: [
-          Container(
-            decoration: BoxDecoration(
-              gradient: quimifyGradient(),
-            ),
-            padding: const EdgeInsets.only(
-              bottom: _bodyRoundedCornersRadius,
-            ),
-            child: widget.header,
+      body: ResponsiveBreakpoints.of(context).smallerThan(MOBILE)
+          ? small
+          : mobile,
+    );
+  }
+}
+
+class MobileSmallView extends StatelessWidget {
+  const MobileSmallView({
+    super.key,
+    required double bodyRoundedCornersRadius,
+    required this.header,
+    required this.body,
+    required bool loadedBannerAd,
+    required Widget? bannerAd,
+  })  : _bodyRoundedCornersRadius = bodyRoundedCornersRadius,
+        _loadedBannerAd = loadedBannerAd,
+        _bannerAd = bannerAd;
+
+  final double _bodyRoundedCornersRadius;
+  final Widget header;
+  final Widget body;
+  final bool _loadedBannerAd;
+  final Widget? _bannerAd;
+
+  @override
+  Widget build(BuildContext context) {
+    return CustomScrollView(
+      slivers: <Widget>[
+        SliverToBoxAdapter(
+          child: _Header(
+            bodyRoundedCornersRadius: _bodyRoundedCornersRadius,
+            header: header,
           ),
-          Expanded(
-            child: Container(
-              clipBehavior: Clip.hardEdge,
-              // To avoid rounded corners overflow
-              decoration: BoxDecoration(
-                color: Theme.of(context).colorScheme.background,
-                borderRadius: const BorderRadius.vertical(
-                  top: Radius.circular(_bodyRoundedCornersRadius),
+        ),
+        SliverList(
+          delegate: SliverChildListDelegate.fixed(
+            [
+              _Body(
+                bodyRoundedCornersRadius: _bodyRoundedCornersRadius,
+                body: body,
+              ),
+              if (_loadedBannerAd) // TODO constant banner?
+                _BannerAd(
+                  bodyRoundedCornersRadius: _bodyRoundedCornersRadius,
+                  bannerAd: _bannerAd,
                 ),
-              ),
-              child: widget.body,
-            ),
+            ],
           ),
-          if (_loadedBannerAd) // TODO constant banner?
-            Padding(
-              padding: const EdgeInsets.only(
-                top: _bodyRoundedCornersRadius, // To counter overlap
-              ),
-              child: SafeArea(
-                top: false,
-                child: _bannerAd!,
-              ),
-            ),
-        ],
+        ),
+      ],
+    );
+  }
+}
+
+class MobileView extends StatelessWidget {
+  const MobileView({
+    super.key,
+    required double bodyRoundedCornersRadius,
+    required this.header,
+    required this.body,
+    required bool loadedBannerAd,
+    required Widget? bannerAd,
+  })  : _bodyRoundedCornersRadius = bodyRoundedCornersRadius,
+        _loadedBannerAd = loadedBannerAd,
+        _bannerAd = bannerAd;
+
+  final double _bodyRoundedCornersRadius;
+  final Widget header;
+  final Widget body;
+  final bool _loadedBannerAd;
+  final Widget? _bannerAd;
+
+  @override
+  Widget build(BuildContext context) {
+    return SignedSpacingColumn(
+      spacing: -_bodyRoundedCornersRadius,
+      stackingOrder: StackingOrder.lastOnTop,
+      children: [
+        _Header(
+          bodyRoundedCornersRadius: _bodyRoundedCornersRadius,
+          header: header,
+        ),
+        Expanded(
+          child: _Body(
+            bodyRoundedCornersRadius: _bodyRoundedCornersRadius,
+            body: body,
+          ),
+        ),
+        if (_loadedBannerAd) // TODO constant banner?
+          _BannerAd(
+            bodyRoundedCornersRadius: _bodyRoundedCornersRadius,
+            bannerAd: _bannerAd,
+          ),
+      ],
+    );
+  }
+}
+
+class _Body extends StatelessWidget {
+  const _Body({
+    required double bodyRoundedCornersRadius,
+    required this.body,
+  }) : _bodyRoundedCornersRadius = bodyRoundedCornersRadius;
+
+  final double _bodyRoundedCornersRadius;
+  final Widget body;
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: MediaQuery.of(context).size.height,
+      child: Container(
+        clipBehavior: Clip.hardEdge,
+        // To avoid rounded corners overflow
+        decoration: BoxDecoration(
+          color: Theme.of(context).colorScheme.background,
+          borderRadius: BorderRadius.vertical(
+            top: Radius.circular(_bodyRoundedCornersRadius),
+          ),
+        ),
+        child: body,
+      ),
+    );
+  }
+}
+
+class _Header extends StatelessWidget {
+  const _Header({
+    required double bodyRoundedCornersRadius,
+    required this.header,
+  }) : _bodyRoundedCornersRadius = bodyRoundedCornersRadius;
+
+  final double _bodyRoundedCornersRadius;
+  final Widget header;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        gradient: quimifyGradient(),
+      ),
+      padding: EdgeInsets.only(
+        bottom: _bodyRoundedCornersRadius,
+      ),
+      child: header,
+    );
+  }
+}
+
+class _BannerAd extends StatelessWidget {
+  const _BannerAd({
+    required double bodyRoundedCornersRadius,
+    required Widget? bannerAd,
+  })  : _bodyRoundedCornersRadius = bodyRoundedCornersRadius,
+        _bannerAd = bannerAd;
+
+  final double _bodyRoundedCornersRadius;
+  final Widget? _bannerAd;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: EdgeInsets.only(
+        top: _bodyRoundedCornersRadius, // To counter overlap
+      ),
+      child: SafeArea(
+        top: false,
+        child: _bannerAd!,
       ),
     );
   }
