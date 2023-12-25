a9e1d5dc69f72f44573c895dfc2d69cb1b642648
Pablo Asenjo Navas-Parejo <pablo@quimify.com>
2023-12-25 16:06:36 +0100

    Fix ads.dart conflict

--- lib/internet/ads/ads.dart
+++ lib/internet/ads/ads.dart
@@ -1,6 +1,7 @@
+import 'dart:developer' as developer;
 import 'dart:io';
+import 'dart:ui';
 
-import 'package:flutter/material.dart';
 import 'package:google_mobile_ads/google_mobile_ads.dart';
 import 'package:quimify_client/internet/ads/env/env.dart';
 
@@ -11,15 +12,19 @@ class Ads {
 
   Ads._internal();
 
-  late String _interstitialUnitId;
+  // Constants:
+
+  static const int _interstitialPeriod = 2; // Minimum attempts before next one
+  static const int _interstitialOffset = 1; // Minimum attempts before 1st one
+
+  // Fields:
+
   late String _bannerUnitId;
+  late String _interstitialUnitId;
 
   InterstitialAd? _nextInterstitial;
 
-  // Constants:
-
-  static const int _interstitialPeriod = 3; // Must be > 1
-  static const int _interstitialOffset = 1; // Must be > 0
+  int _interstitialFreeAttempts = _interstitialPeriod - _interstitialOffset;
 
   // Initialize:
 
@@ -27,89 +32,88 @@ class Ads {
     MobileAds.instance.initialize();
 
     if (Platform.isAndroid) {
-      _interstitialUnitId = Env.androidInterstitialUnitId;
       _bannerUnitId = Env.androidBannerUnitId;
+      _interstitialUnitId = Env.androidInterstitialUnitId;
     } else {
-      _interstitialUnitId = Env.iosInterstitialUnitId;
       _bannerUnitId = Env.iosBannerUnitId;
+      _interstitialUnitId = Env.iosInterstitialUnitId;
     }
-    final params = ConsentRequestParameters();
-    ConsentInformation.instance.requestConsentInfoUpdate(params, () async {
-      if (await ConsentInformation.instance.isConsentFormAvailable()) {
-        loadConsentForm();
-      }
-    }, (error) {
-      //TODO: Manage error
-    });
+
+    ConsentInformation.instance.requestConsentInfoUpdate(
+      ConsentRequestParameters(),
+      () async {
+        if (await ConsentInformation.instance.isConsentFormAvailable()) {
+          _loadConsentForm();
+        }
+      },
+      (error) => developer.log(error.message),
+    );
+
+    _loadInterstitial();
   }
 
   // Private:
 
-  int _interstitialAttempts = _interstitialOffset;
-
-  bool _timeToLoadInterstitial() =>
-      (_interstitialAttempts + 1) % _interstitialPeriod == 0;
-
-  bool _timeToShowInterstitial() =>
-      _interstitialAttempts % _interstitialPeriod == 0;
-
-  loadConsentForm() {
-    ConsentForm.loadConsentForm((consentForm) async {
-      final status = await ConsentInformation.instance.getConsentStatus();
-      if (status == ConsentStatus.required) {
-        consentForm.show((formError) {
-          // Call this method again, if the user has already selected an
-          // option the message will not be displayed again.
-          loadConsentForm();
-        });
-      }
-    }, (FormError? error) {
-      //TODO: Handle error
-    });
-  }
-
-  _loadInterstitial() async {
-    await InterstitialAd.load(
-      adUnitId: _interstitialUnitId,
-      request: const AdRequest(),
-      adLoadCallback: InterstitialAdLoadCallback(
-        onAdLoaded: (ad) {
-          _nextInterstitial = ad;
-
-          ad.fullScreenContentCallback = FullScreenContentCallback(
-            onAdFailedToShowFullScreenContent: (ad, err) => ad.dispose(),
-            onAdDismissedFullScreenContent: (ad) => ad.dispose(),
-          );
-        },
-        onAdFailedToLoad: (_) => {},
-      ),
+  _loadConsentForm() {
+    ConsentForm.loadConsentForm(
+      (consentForm) async {
+        final status = await ConsentInformation.instance.getConsentStatus();
+        if (status == ConsentStatus.required) {
+          consentForm.show((formError) {
+            // Call this method again, if the user has already selected an
+            // option the message will not be displayed again.
+            _loadConsentForm();
+          });
+        }
+      },
+      (FormError? error) => developer.log(error?.message ?? ''),
     );
   }
 
+  _loadInterstitial() => InterstitialAd.load(
+        adUnitId: _interstitialUnitId,
+        request: const AdRequest(),
+        adLoadCallback: InterstitialAdLoadCallback(
+          onAdLoaded: (ad) {
+            _nextInterstitial = ad;
+
+            ad.fullScreenContentCallback = FullScreenContentCallback(
+              onAdFailedToShowFullScreenContent: (ad, err) => ad.dispose(),
+              onAdDismissedFullScreenContent: (ad) => ad.dispose(),
+            );
+          },
+          onAdFailedToLoad: (_) => {},
+        ),
+      );
+
   // Public:
-
   showInterstitial() {
-    _interstitialAttempts += 1;
+    if (_nextInterstitial != null) {
+      if (_interstitialFreeAttempts >= _interstitialPeriod) {
+        _nextInterstitial!.show();
+        _nextInterstitial = null;
 
-    if (_timeToLoadInterstitial()) {
+        _loadInterstitial();
+        _interstitialFreeAttempts = 0;
+      } else {
+        _interstitialFreeAttempts += 1;
+      }
+    } else {
       _loadInterstitial();
-    } else if (_timeToShowInterstitial()) {
-      _nextInterstitial?.show();
+      _interstitialFreeAttempts += 1;
     }
   }
 
-  loadBanner(Size size, void Function(Ad) onLoaded) async {
-    BannerAd(
-      adUnitId: _bannerUnitId,
-      request: const AdRequest(),
-      size: AdSize(
-        width: size.width.toInt(),
-        height: size.height.toInt(),
-      ),
-      listener: BannerAdListener(
-        onAdLoaded: (ad) => onLoaded(ad),
-        onAdFailedToLoad: (ad, err) => ad.dispose(),
-      ),
-    ).load();
-  }
+  loadBanner(Size size, void Function(Ad) onLoaded) => BannerAd(
+        adUnitId: _bannerUnitId,
+        request: const AdRequest(),
+        size: AdSize(
+          width: size.width.toInt(),
+          height: size.height.toInt(),
+        ),
+        listener: BannerAdListener(
+          onAdLoaded: (ad) => onLoaded(ad),
+          onAdFailedToLoad: (ad, err) => ad.dispose(),
+        ),
+      ).load();
 }
