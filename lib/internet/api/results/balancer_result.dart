64034f6258078f2f2e84940548ffa4496251dc21
Matthew Horne Negro <matthew@quimify.com>
2024-04-23 19:21:21 +0200

    BalancerResult class implemented

---
+++ lib/internet/api/results/balancer_result.dart
@@ -0,0 +1,25 @@
+import 'dart:convert';
+
+class BalancerResult {
+  final bool present;
+  final String originalEquation;
+  final String balancedEquation;
+  final String error;
+
+  BalancerResult(
+    this.present,
+    this.originalEquation,
+    this.balancedEquation,
+    this.error,
+  );
+
+  factory BalancerResult.fromJson(String body, String formula) {
+    var json = jsonDecode(body);
+    return BalancerResult(
+      json['present'],
+      json['originalEquation'],
+      json['balancedEquation'],
+      json['error'],
+    );
+  }
+}
