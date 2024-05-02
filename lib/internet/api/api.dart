fbfb5f4988fee4766b17afa503a6b4147c2a0035
Pablo Asenjo Navas-Parejo <pablo@quimify.com>
2024-05-02 18:23:04 +0200

    Update client version 12 -> 13, 4.2.0 -> 4.2.1 and Android build code

--- lib/internet/api/api.dart
+++ lib/internet/api/api.dart
@@ -24,7 +24,7 @@ class Api {
   static const _httpStatusCodeOk = 200;
 
   static const _apiVersion = 6;
-  static const _clientVersion = 12;
+  static const _clientVersion = 13;
   static const _authority = 'api.quimify.com';
   static const _mirrorAuthority = 'api2.quimify.com';
 
