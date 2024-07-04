d1862deed702ae9ec156daae755e9f3f1c4adf35
Matthew Horne <matthew@quimify.com>
2024-07-04 18:34:10 +0200

    google_sign_in_api.dart implemented

--- lib/internet/api/sign-in/google_sign_in_api.dart
+++ lib/internet/api/sign-in/google_sign_in_api.dart
@@ -1,9 +1,14 @@
 import 'package:google_sign_in/google_sign_in.dart';
 
-class GoogleSignInApi{
+class GoogleSignInApi {
   static final _googleSignIn = GoogleSignIn();
 
   static Future<GoogleSignInAccount?> login() => _googleSignIn.signIn();
 
+  static Future<GoogleSignInAccount?> loginSilently() => _googleSignIn.signInSilently();
+
+  static Future<bool> isLoggedIn() => _googleSignIn.isSignedIn();
+
   static Future logout() => _googleSignIn.disconnect();
+
 }
