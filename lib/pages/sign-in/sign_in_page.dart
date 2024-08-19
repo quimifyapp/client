445f779f3a2148e61ed9ab6c1725dd63f230c556
Matthew Horne <matthew@quimify.com>
2024-08-19 11:33:42 +0200

    Reformat code with 'dart format'

--- lib/pages/sign-in/sign_in_page.dart
+++ lib/pages/sign-in/sign_in_page.dart
@@ -21,81 +21,82 @@ class SignInPage extends StatelessWidget {
           padding: const EdgeInsets.symmetric(horizontal: 40.0),
           child: LayoutBuilder(
             builder: (BuildContext context, BoxConstraints constraints) {
-              return Center(  // Added Center widget for vertical centering
-                  child: Column(
+              return Center(
+                // Added Center widget for vertical centering
+                child: Column(
                   mainAxisAlignment: MainAxisAlignment.center,
                   children: [
-                  ConstrainedBox(
-                  constraints: BoxConstraints(
-                  maxHeight: constraints.maxHeight * 0.3,
-                  maxWidth: constraints.maxWidth * 0.8,
-              ),
-              child: Image.asset(
-              'assets/images/logo.png',
-              height: 175, // Set a fixed height for the image
-              ),
-              ),
-              const SizedBox(height: 50),
-
-                  // Google Sign-In button
-                  ElevatedButton.icon(
-                    onPressed: () async {
-                      final user = await GoogleSignInApi.login();
-                      if (user != null) {
-                        Navigator.of(context).pushReplacement(MaterialPageRoute(
-                            builder: (context) =>
-                                HomePage(
-                                    clientResult: clientResult, user: user)));
-                      }
-                    },
-                    icon: Image.asset(
-                        'assets/images/icons/google-logo.png', height: 24),
-                    label: const Text('Iniciar Sesión con Google'),
-                    style: ElevatedButton.styleFrom(
-                      foregroundColor: Colors.black,
-                      backgroundColor: Colors.white,
-                      minimumSize: const Size(double.infinity, 50),
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(30),
+                    ConstrainedBox(
+                      constraints: BoxConstraints(
+                        maxHeight: constraints.maxHeight * 0.3,
+                        maxWidth: constraints.maxWidth * 0.8,
+                      ),
+                      child: Image.asset(
+                        'assets/images/logo.png',
+                        height: 175, // Set a fixed height for the image
                       ),
                     ),
-                  ),
+                    const SizedBox(height: 50),
 
-                  const SizedBox(height: 20),
-
-                  // Apple Sign-In button (assuming you have Apple sign-in set up)
-                  ElevatedButton.icon(
-                    onPressed: () async {
-                      // TODO Handle Apple sign-in
-                      final user = await GoogleSignInApi.login();
-                      if (user != null) {
-                        Navigator.of(context).pushReplacement(MaterialPageRoute(
-                            builder: (context) =>
-                                HomePage(
-                                    clientResult: clientResult, user: user)));
-                      }
-                    },
-                    icon: const Icon(
-                        Icons.apple, color: Colors.white, size: 24),
-                    label: const Text('Iniciar Sesión con Apple'),
-                    style: ElevatedButton.styleFrom(
-                      foregroundColor: Colors.white,
-                      backgroundColor: Colors.black,
-                      minimumSize: const Size(double.infinity, 50),
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(30),
+                    // Google Sign-In button
+                    ElevatedButton.icon(
+                      onPressed: () async {
+                        final user = await GoogleSignInApi.login();
+                        if (user != null) {
+                          Navigator.of(context).pushReplacement(
+                              MaterialPageRoute(
+                                  builder: (context) => HomePage(
+                                      clientResult: clientResult, user: user)));
+                        }
+                      },
+                      icon: Image.asset('assets/images/icons/google-logo.png',
+                          height: 24),
+                      label: const Text('Iniciar Sesión con Google'),
+                      style: ElevatedButton.styleFrom(
+                        foregroundColor: Colors.black,
+                        backgroundColor: Colors.white,
+                        minimumSize: const Size(double.infinity, 50),
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(30),
+                        ),
                       ),
                     ),
-                  ),
 
-                  const SizedBox(height: 50),
+                    const SizedBox(height: 20),
 
-                  Image.asset(
-                    'assets/images/branding.png',
-                    height: 25,
-                  ),
+                    // Apple Sign-In button (assuming you have Apple sign-in set up)
+                    ElevatedButton.icon(
+                      onPressed: () async {
+                        // TODO Handle Apple sign-in
+                        final user = await GoogleSignInApi.login();
+                        if (user != null) {
+                          Navigator.of(context).pushReplacement(
+                              MaterialPageRoute(
+                                  builder: (context) => HomePage(
+                                      clientResult: clientResult, user: user)));
+                        }
+                      },
+                      icon: const Icon(Icons.apple,
+                          color: Colors.white, size: 24),
+                      label: const Text('Iniciar Sesión con Apple'),
+                      style: ElevatedButton.styleFrom(
+                        foregroundColor: Colors.white,
+                        backgroundColor: Colors.black,
+                        minimumSize: const Size(double.infinity, 50),
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(30),
+                        ),
+                      ),
+                    ),
+
+                    const SizedBox(height: 50),
+
+                    Image.asset(
+                      'assets/images/branding.png',
+                      height: 25,
+                    ),
                   ],
-                  ),
+                ),
               );
             },
           ),
